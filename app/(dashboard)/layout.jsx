e915dbdd5bac0846b3d0b04292bd90f3fd393099
adilkhan2110 <beingadilkhan@gmail.com>
2025-07-15 23:51:40 +0530

    "issue fixed"

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -10,12 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="antialiased">
-        <Header /> {children}
-      </body>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
 
-import Header from "@/components/Header";
-import "./globals.css";
+ 
+ 
