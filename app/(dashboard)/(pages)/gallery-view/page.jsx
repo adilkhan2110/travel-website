f929fbd40906bc9679299850fe35d0f5fffd5e8b
adilkhan2110 <beingadilkhan@gmail.com>
2025-07-17 12:20:28 +0530

    code merege

--- app/(dashboard)/(pages)/gallery-view/page.jsx
+++ app/(dashboard)/(pages)/gallery-view/page.jsx
@@ -147,7 +147,7 @@ export default function GalleryPage() {
   return (
     <div  >
       <div className="flex justify-between items-center mb-4">
-        <h2 className="text-2xl font-bold">Gallery</h2>
+        <h2 className="text-2xl font-bold">Gallery 1</h2>
         <Button variant="contained" className="" onClick={handleOpen}>
           Add New Item
         </Button>
